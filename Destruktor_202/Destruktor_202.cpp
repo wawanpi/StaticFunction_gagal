a90c4b61bfead16abf143ba94ace668441c775db
wawanpi <muhaguskurniawan@gmail.com>
2023-05-15 21:10:21 +0700

    done bang

--- Destruktor_202/Destruktor_202.cpp
+++ Destruktor_202/Destruktor_202.cpp
@@ -40,7 +40,7 @@ void angka::isiData() {
 
 int main() {
 	angka belajarcpp(3);	//Constructor Dipanggil
-	angka* ptrBelajarcpp = new angka(5);	//Constructor
+	angka* ptrBelajarcpp = new angka(5);	//Constructor 
 	delete ptrBelajarcpp;	//Destructor Dipanggil
 
 	return 0;
